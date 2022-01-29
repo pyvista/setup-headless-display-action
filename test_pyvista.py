53ee744700e855d487d4277d38c004b4f9e16262
banesullivan <banesullivan@gmail.com>
2022-01-29 14:51:54 -0700

    Improve test

--- test_pyvista.py
+++ test_pyvista.py
@@ -4,6 +4,6 @@ from pyvista.plotting import system_supports_plotting
 
 print(f'system_supports_plotting: {system_supports_plotting()}')
 assert system_supports_plotting()
-pyvista.OFF_SCREEN = True
+# pyvista.OFF_SCREEN = True  # should be set by Action in Env
 sphere = pyvista.Sphere()
 pyvista.plot(sphere, screenshot='sphere.png')
