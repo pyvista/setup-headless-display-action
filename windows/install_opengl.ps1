12d2f0d53223d70c492f9cbb62855d7739c624f1
Bane Sullivan <bane.sullivan@kitware.com>
2022-11-16 13:18:20 -0700

    debug

--- windows/install_opengl.ps1
+++ windows/install_opengl.ps1
@@ -1,10 +1,11 @@
 function InstallDLL ($name) {
+    $scriptpath = Split-Path $MyInvocation.MyCommand.Path -Parent
     $filepath = "C:\Windows\system32\$name"
-    $sourcepath = "${env:ActionPath}\windows\mesa-$version\$name"
+    $sourcepath = "$scriptpath\windows\mesa-$version\$name"
     takeown /F $filepath /A
     icacls $filepath /grant "${env:ComputerName}\${env:UserName}:F"
     Remove-item -LiteralPath $filepath
-    Write-Host "${env:ActionPath}"
+    Write-Host $scriptpath
     Write-Host "Installing to" $filepath "from" $sourcepath
     Copy-Item $sourcepath -Destination $filepath
 }
@@ -17,6 +18,8 @@ function InstallMesaOpenGL ($version) {
 
 
 function main () {
+    $scriptpath = $MyInvocation.MyCommand.Path
+    Write-Output "Path of the script : $scriptpath"
     InstallMesaOpenGL "22.0.1"
 }
 
