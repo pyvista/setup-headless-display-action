2af0866217e9acda3de561d9db82948670a7ce6a
Bane Sullivan <bane.sullivan@kitware.com>
2022-11-16 12:30:07 -0700

    use script

---
+++ windows/install.ps1
@@ -0,0 +1,15 @@
+function InstallMesaOpenGL ($version) {
+    $filepath = "C:\Windows\system32\opengl32.dll"
+    takeown /F $filepath /A
+    icacls $filepath /grant "${env:ComputerName}\${env:UserName}:F"
+    Remove-item -LiteralPath $filepath
+    Write-Host "Installing to" $filepath
+    Copy-Item "$action_path\windows\mesa-$version\opengl32.dll" -Destination $filepath
+}
+
+
+function main () {
+    InstallMesaOpenGL "22.0.1"
+}
+
+main
