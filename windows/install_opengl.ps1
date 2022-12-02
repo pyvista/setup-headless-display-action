355cbcc79f97763ef3ba4d53086fff603a1e5c17
Bane Sullivan <bane.sullivan@kitware.com>
2022-12-02 10:46:02 -0700

    Try download

--- windows/install_opengl.ps1
+++ windows/install_opengl.ps1
@@ -1,26 +1,41 @@
-function InstallDLL ($name) {
-    $scriptpath = Split-Path $MyInvocation.MyCommand.Path -Parent
-    $filepath = "C:\Windows\system32\$name"
-    $sourcepath = "$scriptpath\windows\mesa-$version\$name"
+# Adapted from VisPy
+$MESA_GL_URL = "https://github.com/pyvista/setup-headless-display-action/raw/mesa/windows/mesa-22.0.1"
+
+function DownloadMesaOpenGL ($architecture) {
+    [Net.ServicePointManager]::SecurityProtocol = 'Tls, Tls11, Tls12'
+    $webclient = New-Object System.Net.WebClient
+    # Download and retry up to 3 times in case of network transient errors.
+    $url = $MESA_GL_URL + "opengl32" + $architecture + ".dll"
+    if ($architecture -eq "32") {
+        $filepath = "C:\Windows\SysWOW64\opengl32.dll"
+    } else {
+        $filepath = "C:\Windows\system32\opengl32.dll"
+    }
     takeown /F $filepath /A
     icacls $filepath /grant "${env:ComputerName}\${env:UserName}:F"
     Remove-item -LiteralPath $filepath
-    Write-Host $scriptpath
-    Write-Host "Installing to" $filepath "from" $sourcepath
-    Copy-Item $sourcepath -Destination $filepath
-}
-
-
-function InstallMesaOpenGL ($version) {
-    InstallDLL "opengl32.dll"
-    # InstallDLL "libglapi.dll"
+    Write-Host "Downloading" $url
+    $retry_attempts = 2
+    for($i=0; $i -lt $retry_attempts; $i++){
+        try {
+            $webclient.DownloadFile($url, $filepath)
+            break
+        }
+        Catch [Exception]{
+            Start-Sleep 1
+        }
+    }
+    if (Test-Path $filepath) {
+        Write-Host "File saved at" $filepath
+    } else {
+        # Retry once to get the error message if any at the last try
+        $webclient.DownloadFile($url, $filepath)
+    }
 }
 
 
 function main () {
-    $scriptpath = $MyInvocation.MyCommand.Path
-    Write-Output "Path of the script : $scriptpath"
-    InstallMesaOpenGL "22.0.1"
+    DownloadMesaOpenGL "64"
 }
 
 main
