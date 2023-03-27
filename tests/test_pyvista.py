d11e26e19fd9daed9c30b23ef9a635f11702a230
Eric Larson <larson.eric.d@gmail.com>
2023-03-27 13:22:48 -0500

    ENH: Add qt and pyvista options

--- tests/test_pyvista.py
+++ tests/test_pyvista.py
@@ -1,4 +1,5 @@
 """Quickly check if VTK off screen plotting works."""
+from pathlib import Path
 import pyvista
 from pyvista.plotting import system_supports_plotting
 
@@ -6,4 +7,5 @@ print(f'system_supports_plotting: {system_supports_plotting()}')
 assert system_supports_plotting()
 # pyvista.OFF_SCREEN = True  # should be set by Action in Env
 sphere = pyvista.Sphere()
-pyvista.plot(sphere, screenshot='sphere.png')
+out_path = Path(__file__).parent / '..' / 'sphere.png'
+pyvista.plot(sphere, screenshot=out_path)
