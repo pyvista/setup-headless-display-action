4cf5c603091e085da8830e1480355ff03f3e171b
Talley Lambert <talley.lambert@gmail.com>
2024-06-24 15:32:25 -0400

    Add vispy test (#17)

---
+++ tests/test_vispy_qt.py
@@ -0,0 +1,21 @@
+"""Quickly check if vispy off screen plotting works."""
+
+import os
+from pathlib import Path
+import numpy as np
+from qtpy.QtWidgets import QApplication
+from vispy import scene
+from PIL import Image
+
+qapp = QApplication([])
+canvas = scene.SceneCanvas(size=(512, 512))
+view = canvas.central_widget.add_view()
+np.random.seed(0)
+vol_data = np.random.rand(64, 64, 64).astype(np.float32)
+image = scene.visuals.Volume(vol_data, cmap="viridis", parent=view.scene)
+view.camera = scene.ArcballCamera()
+canvas.show()
+
+fname = f'{os.environ['MATRIX_OS']}-{os.environ['MATRIX_QT']}-vispy-volume.png'
+out_path = Path(__file__).parent.parent / fname
+Image.fromarray(canvas.render()).save(out_path)
