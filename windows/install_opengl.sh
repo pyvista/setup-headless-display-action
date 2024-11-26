3fdc3caa3cbe3763eb1eba2a835a9499e920978a
Eric Larson <larson.eric.d@gmail.com>
2024-11-26 12:36:43 -0500

    FIX: Path

--- windows/install_opengl.sh
+++ windows/install_opengl.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -exo pipefail
-
-curl -LO https://github.com/pal1000/mesa-dist-win/releases/download/24.2.7/mesa3d-24.2.7-release-msvc.7z
-7z x mesa3d-24.2.7-release-msvc.7z
-mv -v mesa3d-24.2.7-release-msvc/x64/* /C/Windows/System32/
+VER="24.2.7"
+NAME="mesa3d-${VER}-release-msvc"
+curl -LO https://github.com/pal1000/mesa-dist-win/releases/download/${VER}/${NAME}.7z
+7z x ${NAME}.7z -o./${NAME}
+ls -alR ${NAME}/
+mv -v ${NAME}/x64/* /C/Windows/System32/
+rm -Rf ${NAME}
 takeown /f "C:\Windows\System32\opengl32.dll"
 icacls "C:\Windows\System32\opengl32.dll" /grant "$USERNAME:F"
