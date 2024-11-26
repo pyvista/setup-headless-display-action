92935caaa8e1d2c762a21ac8c97f463bd4f69698
Eric Larson <larson.eric.d@gmail.com>
2024-11-26 12:47:49 -0500

    FIX

--- windows/install_opengl.sh
+++ windows/install_opengl.sh
@@ -5,7 +5,6 @@ VER="24.2.7"
 NAME="mesa3d-${VER}-release-msvc"
 curl -LO https://github.com/pal1000/mesa-dist-win/releases/download/${VER}/${NAME}.7z
 7z x ${NAME}.7z -o./${NAME}
-ls -alR ${NAME}/
 mv -v ${NAME}/x64/* /C/Windows/System32/
 rm -Rf ${NAME}
 # takeown "/f" "C:\Windows\System32\opengl32.dll"
