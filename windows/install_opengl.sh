d6dd86a3f0ccdab45713646ea93dd4b636f7c996
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-12-02 12:38:59 +0100

    fix: use Mesa3D system-wide installer (#2)
    * feat: fix
    
    * feat: fix
    
    * final fixes

--- windows/install_opengl.sh
+++ windows/install_opengl.sh
@@ -11,8 +11,9 @@ fi
 NAME="mesa3d-${MESA3D_VERSION}-release-msvc"
 curl -LO https://github.com/pal1000/mesa-dist-win/releases/download/${MESA3D_VERSION}/${NAME}.7z
 7z x ${NAME}.7z -o./${NAME}
-mv -v ${NAME}/x64/* /C/Windows/System32/
-rm -Rf ${NAME}
+# Run systemwidedeploy.cmd file: option 1) Install OpenGL drivers & 7) Update system-wide deployment
+cmd.exe //c "${NAME}\systemwidedeploy.cmd 1"
+cmd.exe //c "${NAME}\systemwidedeploy.cmd 7"
 # takeown "/f" "C:\Windows\System32\opengl32.dll"
 # icacls "C:\Windows\System32\opengl32.dll" /grant "$USERNAME:F"
 ls -alt /C/Windows/System32/opengl32.dll
