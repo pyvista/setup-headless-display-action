13ecd9b99114910aa2196458dac52cbbb3e3eed9
Talley Lambert <talley.lambert@gmail.com>
2025-02-10 09:53:39 -0500

    add retries and no-revoke to mesa3d curl (#26)

--- windows/install_opengl.sh
+++ windows/install_opengl.sh
@@ -9,7 +9,7 @@ if [ -z "${MESA3D_VERSION}" ]; then
 fi
 
 NAME="mesa3d-${MESA3D_VERSION}-release-msvc"
-curl -LO https://github.com/pal1000/mesa-dist-win/releases/download/${MESA3D_VERSION}/${NAME}.7z
+curl -LO --retry 3 --ssl-no-revoke https://github.com/pal1000/mesa-dist-win/releases/download/${MESA3D_VERSION}/${NAME}.7z
 7z x ${NAME}.7z -o./${NAME}
 # Run systemwidedeploy.cmd file: option 1) Install OpenGL drivers & 7) Update system-wide deployment
 cmd.exe //c "${NAME}\systemwidedeploy.cmd 1"
