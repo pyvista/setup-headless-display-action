dbce7c2fa4bb2a5b69bff030d6352a564600246d
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2025-04-22 18:06:15 +0200

    feat: install osmesa off-screen renderer on Windows runners (#29)
    Co-authored-by: Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>

--- windows/install_opengl.sh
+++ windows/install_opengl.sh
@@ -11,10 +11,20 @@ fi
 NAME="mesa3d-${MESA3D_VERSION}-release-msvc"
 curl -LO --retry 3 --ssl-no-revoke https://github.com/pal1000/mesa-dist-win/releases/download/${MESA3D_VERSION}/${NAME}.7z
 7z x ${NAME}.7z -o./${NAME}
-# Run systemwidedeploy.cmd file: option 1) Install OpenGL drivers & 7) Update system-wide deployment
+# Run systemwidedeploy.cmd file:
+#  option 1) Install OpenGL drivers 
+#  option 5) Mesa3D off-screen render driver gallium version (osmesa gallium)
+#  option 7) Update system-wide deployment
 cmd.exe //c "${NAME}\systemwidedeploy.cmd 1"
+if [ "${MESA3D_OFFSCREEN}" == "true" ]; then
+  echo "Installing off-screen render driver gallium version (osmesa gallium)"
+  cmd.exe //c "${NAME}\systemwidedeploy.cmd 5"
+fi
 cmd.exe //c "${NAME}\systemwidedeploy.cmd 7"
 rm -Rf ${NAME}
 # takeown "/f" "C:\Windows\System32\opengl32.dll"
 # icacls "C:\Windows\System32\opengl32.dll" /grant "$USERNAME:F"
 ls -alt /C/Windows/System32/opengl32.dll
+if [ "${MESA3D_OFFSCREEN}" == "true" ]; then
+  ls -alt /C/Windows/System32/osmesa.dll
+fi
